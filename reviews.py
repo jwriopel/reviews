9a0d47894f88a67154c52dcb39c4e3c3b4b34bbc
Joe Riopel <goon12@gmail.com>
2014-12-15 21:33:53 -0500

    added Parser class to simplify the interface to reviews and parses out the 'watchables' from the file

--- reviews.py
+++ reviews.py
@@ -7,6 +7,7 @@ import re
 
 
 FILES_RE = re.compile(r"[-|+]{3}\s+([\w|\W][^\t]*)\s*\S*")
+HUNK_RE = re.compile(r"^@@\s([-|\+]\d+,\d+)\s(\+\d+,\d+)\s@@")
 
 
 def files(u_diff):
@@ -27,3 +28,108 @@ def files(u_diff):
             diff_files.append(search_res.group(1))
 
     return diff_files
+
+
+def hunks(u_diff):
+    """
+    Parse the hunks out of the unified diff.
+
+    :param u_diff: File-like object containing the unified diff.
+    :return: List containig pairs of tuples with hunk information.
+    """
+    d_hunks = []
+
+    clean_lines = (l.strip() for l in u_diff.readlines())
+    hunk_lines = (l for l in clean_lines if l.startswith("@@"))
+
+    for line in hunk_lines:
+        h_res = HUNK_RE.search(line)
+        if h_res:
+            d_hunks.append(h_res.groups())
+
+    return d_hunks
+
+
+class Parser(object):
+    """
+    Type used for parsing diff files and extracting watchable hunks of files.
+    """
+
+    def __init__(self, unified_diff):
+        """
+        Create an instance of the Parser.
+
+        :param unified_diff: File-like object containing the contents of a
+            unified diff.
+        """
+        self.unified_diff = unified_diff
+
+
+    def is_header(self, line):
+        """ True if the line is a 'header' - meaning file """
+        header_markers = ("---", "+++")
+        return line[:3] in header_markers
+
+    def extract_filename(self, line):
+        """
+        extract the file name from the header line.
+
+        :param line: A header line from the diff.
+        :return: String containing the file name.
+        """
+        search_res = FILES_RE.search(line)
+        f_name = None
+
+        if search_res:
+            f_name = search_res.group(1)
+
+        return f_name
+
+
+    def __hunk_ints(self, hunk_info):
+        """
+        Convert the tuple of hunk info from strings to ints
+
+        :param hunk_pair: String containing the raw line information for a
+            hunk ("xxx,yyy").
+        :return: Tuple containig hunk info as ints (xxx, yyy).
+        """
+        return tuple(int(i) for i in hunk_info.split(","))
+
+
+    def hunks(self, lines):
+        """
+        Parse the hunks from the list of lines, until EOF of another
+        file_header.
+
+        :param lines: List of lines to find the hunks from.
+        """
+        file_hunks = []
+        for line in lines:
+            if self.is_header(line):
+                break
+            elif line.startswith("@@"):
+                h_res = HUNK_RE.search(line)
+                file_hunks.append((
+                    self.__hunk_ints(h_res.group(1)),
+                    self.__hunk_ints(h_res.group(2))
+                ))
+
+        return file_hunks
+
+    def parse(self):
+        """
+        Parse the unified diff into a dictionary containing the
+        watchables.
+        """
+        lines = [line.strip() for line in self.unified_diff.readlines()]
+        watchables = dict()
+        for l_no, line in enumerate(lines):
+
+            if self.is_header(line) and line.startswith("--- "):
+                file_name = self.extract_filename(line)
+                watchables[file_name] = self.hunks(lines[l_no+2:])
+
+                continue
+
+        return watchables
--- tests/test_reviews.py
+++ tests/test_reviews.py
@@ -8,6 +8,12 @@ import pytest
 import reviews
 
 
+HEADER_LINES = [
+    "--- from_file	2014-12-02 21:16:58.331853527 -0500",
+    "+++ to_file	2014-12-02 21:21:43.235849519 -0500"
+]
+
+
 def read_diff(file_name):
     """ read the diff, at `path`, into a file-like object """
 
@@ -43,15 +49,94 @@ def test_multiple_files():
 
     expected = [
             "a/requests/__init__.py",
-            "b/requests/__init__.py",
             "a/test_requests.py",
-            "b/test_requests.py",
             "a/HISTORY.rst",
-            "b/HISTORY.rst"
     ]
-    
-    actuals = sorted(reviews.files(read_diff("multi_file_diff")))
 
-    assert actuals == sorted(expected)
+    parser = reviews.Parser(read_diff("multi_file_diff"))
+    watchables = parser.parse()
+
+    actuals = watchables.keys()
+    assert sorted(actuals) == sorted(expected)
 
 
+@pytest.mark.parametrize("h_line", HEADER_LINES)
+def test_is_header(h_line):
+    """ test for the Parser.is_header method """
+    parser = reviews.Parser(None)
+    assert parser.is_header(h_line)
+
+
+def compare_watchables(actual, expected):
+    """ Assert that the actual watchables are the same as the expected """
+    actual_files = actual.keys()
+    for exp_file in expected:
+        exp_hunk = expected[exp_file]
+
+        assert exp_file in actual_files
+        actual_hunk = actual[exp_file]
+
+        assert actual_hunk == exp_hunk
+
+
+@pytest.mark.parametrize("h_line", HEADER_LINES)
+def test_extract_filename(h_line):
+    """ test that the filenames from a header is properly parsed """
+    parser = reviews.Parser(simple_diff)
+    file_name = parser.extract_filename(h_line)
+
+    if h_line.startswith("---"):
+        assert file_name == "from_file"
+    else:
+        assert file_name == "to_file"
+
+
+def test_parse_hunks(simple_diff):
+    """ test the parser can parse the hunks properly """
+
+    expected = [((-2, 9), (+2, 9))]
+    parser = reviews.Parser(None)
+
+    lines = [l.strip() for l in simple_diff.readlines()]
+    actual_hunks = parser.hunks(lines[2:])
+    assert len(actual_hunks) == len(expected)
+
+    for actual_hunk in actual_hunks:
+        assert actual_hunk in expected
+
+
+def test_build_simple_watchable(simple_diff):
+    """ use the Parser to parse out the 'watchable' things  in diff """
+
+    parser = reviews.Parser(simple_diff)
+    expected = {
+            "from_file": [((-2, 9), (+2, 9))]
+    }
+    actual = parser.parse()
+    compare_watchables(actual, expected)
+
+
+def test_multi_file_watchables():
+    """
+    Test that the watchables are parsed properly, from a diff that contains
+    changes from multiple files.
+    """
+
+    expected = {
+            "a/requests/__init__.py": [((-42, 8), (+42, 8))],
+            "a/HISTORY.rst": [((-3, 6), (+3, 41))],
+            "a/test_requests.py": [
+                ((-258, 7), (258, 7)),
+                ((-1008, 12), (1008, 12)),
+                ((-1350, 7), (1350, 7)),
+                ((-1450, 7), (1450, 7)),
+                ((-1534, 12), (1534, 12))
+            ]
+    }
+
+    parser = reviews.Parser(read_diff("multi_file_diff"))
+    actuals = parser.parse()
+
+    assert len(actuals) == len(expected)
+
+    compare_watchables(actuals, expected)
